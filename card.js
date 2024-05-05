da76dafa6961d0c9ab44dc6981845df066960357
nishitha <nishi.samudrala@gmail.com>
2024-05-05 19:52:48 +0530

    Changes to be committed: 	modified:   card.html 	modified:   card.js

---
+++ card.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".slide-content",{
+    slidesPerView:3,
+    spaceBetween:30,
+    slidesPerGroup:3,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    navigation:{
+        nextE1:".swiper-button-next",
+        prevE1:".swiper-button-prev",
+
+    },
+});
